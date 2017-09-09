ddd1093e39a85b82b235b6086eea23cb645064b7
Will Dixon <wdixon23192@gmail.com>
2017-09-09 18:06:03 -0400

    imported unity and added basic level

---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        //moving down does nothing
+        if(move.y < 0) {
+            move.Set(move.x, 0, 0);
+        }
+        //jump
+        if (move.y > 0) {
+            move.Set(move.x, 2*move.y, 0);
+        }
+            transform.position += move * speed * Time.deltaTime;
+
+    }
+}
