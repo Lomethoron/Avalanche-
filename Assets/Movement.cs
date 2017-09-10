9b620d059cb6c04cb242197344f9dab9b1424147
Will Dixon <wdixon23192@gmail.com>
2017-09-09 20:23:54 -0400

    Converted movement from transform to force

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,25 +3,32 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-    float speed = 1.0f;
+    public float speed = 6.0f;
+    public bool collided = false;
+    public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        //moving down does nothing
-        if(move.y < 0) {
-            move.Set(move.x, 0, 0);
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        if (Input.GetButtonDown("Jump") && collided) {
+            move.y = 100.0f;
         }
-        //jump
-        if (move.y > 0) {
-            move.Set(move.x, 2*move.y, 0);
-        }
-            transform.position += move * speed * Time.deltaTime;
+        rb.AddForce(move * speed);
 
+    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        print("Entered collision");
+        collided = true;
+    }
+    void OnCollisionStay2D(Collision2D collision) {
+        collided = true;
+    }
+    void OnCollisionExit2D() {
+        collided = false;
     }
 }
